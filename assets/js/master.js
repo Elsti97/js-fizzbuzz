5f28677e5624cf2fe2a6d365f56646a5b95f8afb
Elsti97 <elsti.shehati@hotmail.it>
2022-10-21 18:22:15 +0200

    Inizio layout finale

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,18 +1,31 @@
-let Boxone = document.getElementById('GinFizz');
+let Boxone = document.getElementById('FizzGerard');
 
-for (let i=1; i <= 100; i++){
+
+for (let i = 1; i <= 100; i++){
+  let quadrato = document.createElement("div");
+  Boxone.append(quadrato);
+
   if(i % 3 == 0 && i % 5 == 0) {
     console.log('FizzBuzz')
+    quadrato.innerText = 'FizzBuzz';
+    quadrato.classList.add ("bg-danger");
+
   }
   else if (i % 3 == 0) {
-    console.log('Fizz')
-  } 
-  else if (i % 5 == 0){
-    console.log('Buzz')
+    console.log('Fizz');
+    quadrato.innerText = 'Fizz';
+    quadrato.classList.add ("bg-success");
+
+  }
+  else if (i % 5 == 0) {
+    console.log('Buzz');
+    quadrato.innerText = 'Buzz';
+    quadrato.classList.add ("bg-warning");
+
   }
   else {
-    console.log(i)
+    console.log(i);
+    quadrato.innerText = `${i}`;
+    quadrato.classList.add ("bg-primary");
   }
-
-  Boxone.innerHTML = "il tuo numero è '(i)'";
 } 
